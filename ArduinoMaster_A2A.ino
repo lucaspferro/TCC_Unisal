0c3cdaadb8b8ba4de0c1a4861ff8d699e9647e52
lucaspferro <79730080+lucaspferro@users.noreply.github.com>
2023-11-10 16:23:22 -0300

    Update ArduinoMaster_A2A.ino

--- ArduinoMaster_A2A.ino
+++ ArduinoMaster_A2A.ino
@@ -2,10 +2,7 @@
 #include <Adafruit_MQTT.h>
 #include <Adafruit_MQTT_Client.h>
 #include <ESP8266WiFi.h>
-#include <A2a.h>
-#define endereco 0x08
 
-A2a arduinoMaster;
 
 const int relayPin = 13; // Pino de controle do relé
 
@@ -18,23 +15,16 @@ unsigned int flowMilliLitres = 0;
 unsigned long totalMilliLitres = 0;
 unsigned long oldTime = 0;
 
-ICACHE_RAM_ATTR void pulseCounter() {
+ void pulseCounter() {
   pulseCount++;
 }
 
 void setup() {
-  Wire.begin(8); // Inicializa o Arduino como escravo com endereço 8
-  Wire.onReceive(receiveEvent); // Configura a função de recebimento
-  Wire.onRequest(sendData); // Configura a função de solicitação
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW); // Garante que o relé esteja inicialmente desligado
-  pinMode(sensorPin, INPUT);
-  digitalWrite(sensorPin, HIGH); // Ativa o pull-up interno
-
-  Serial.begin(115200);
-
   Wire.begin();
 
+  Serial.begin(115200);
   attachInterrupt(digitalPinToInterrupt(sensorPin), pulseCounter, FALLING);
 }
 
@@ -42,7 +32,6 @@ void loop() {
 // Leia a tensão do sensor
   int sensorValue = analogRead(sensorPin);
   float voltage = (sensorValue / 1023.0) * 5.0; // Converta o valor para tensão (5V como referência)
-
   Serial.print("Tensão lida: ");
   Serial.print(voltage, 2); // Exibe a tensão com duas casas decimais
   Serial.println(" V");
@@ -62,30 +51,13 @@ void loop() {
     Serial.println(" mL");
     pulseCount = 0;
   }
-  arduinoMaster.varWireWrite(0,(totalMilliLitres));
-  delay(100);
-//--------------------
 
-  arduinoMaster.varWireWrite(2, (voltage));
-  delay(100);
- //-------------------
+  Wire.beginTransmission(8);  // Endereço I2C do ESP8266
+  Wire.write("SENSORES:");    // Identificador de dados dos sensores
+  Wire.write((byte*)&tensao, sizeof(voltage));  // Envia a tensão como um array de bytes
+  Wire.write((byte*)&fluxo, sizeof(flowRate));    // Envia o fluxo como um array de bytes
+  Wire.endTransmission();
 
- //arduinoMaster.varWireWrite(6, (Irms));
- //arduinoMaster.begin(endereco);
- delay(100);
- //-------------------
-
- //arduinoMaster.varWireWrite(4, (FP));
-  delay(100);
-
-
- arduinoMaster.begin(endereco);
- arduinoMaster.onReceive(receberDados);
- arduinoMaster.onRequest(enviarDados);
-// arduinoMaster.endTransmission();
- 
-  delay (2000);
-  Serial.print("***************");
 return 0;
 
 
@@ -101,21 +73,3 @@ void receiveEvent(int bytes) {
   }
 }
 
-void sendData() {
-  // Quando o Arduino Mestre solicitar dados, envie a tensão lida
-  int sensorValue = analogRead(sensorPin);
-  float voltage = (sensorValue / 1023.0) * 5.0; // Converta o valor para tensão (5V como referência)
-
-  // Envie os dados para o Arduino Mestre
-  byte data[4];
-  memcpy(data, &voltage, sizeof(voltage));
-  Wire.write(data, sizeof(data));
-}
-
-void receberDados(){
-  arduinoMaster.receiveData();
- }
-
-void enviarDados(){
-  arduinoMaster.sendData();
-  }
